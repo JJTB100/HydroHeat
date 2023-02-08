8bb7b1d39454b8e3d57481b575d1e30a41dd9967
Jonathan Broster <17BrostJ@fulford.york.sch.uk>
2023-02-08 10:16:16 +0000

    charts and colours be like L

--- lib.js
+++ lib.js
@@ -1,8 +1,9 @@
 var slider = document.getElementById("myRange")
 var flownum = document.getElementById("flow-rate-num")
-var flowRate = 0;
+var flowRate = 50;
 var plus = document.getElementById("plus");
 var minus = document.getElementById("minus");
+updateFlow()
 
 slider.oninput = function() {
     flowRate = this.value;
@@ -30,3 +31,36 @@ slider.oninput = function() {
     flownum.innerText = flowRate.toString();
     slider.value = flowRate;
   }
+
+google.charts.load('current', {'packages':['gauge']});
+    google.charts.setOnLoadCallback(drawChart);
+    
+    function drawChart() {
+
+      var data = google.visualization.arrayToDataTable([
+        ['Label', 'Value'],
+        ['Temp', 80],
+        ['Pool Temp', 80]
+      ]);
+
+      var options = {
+        width: window.innerWidth,
+        redFrom: 90, redTo: 100, redColor: getComputedStyle(document.documentElement).getPropertyValue('--main-color'),
+        yellowFrom:75, yellowTo: 90,
+        minorTicks: 5
+      };
+
+      var chart = new google.visualization.Gauge(document.getElementById('temp-chart'));
+
+      chart.draw(data, options);
+
+      setInterval(function() {
+        data.setValue(0, 1, 40 + Math.round(60 * Math.random()));
+        chart.draw(data, options);
+      }, 1000);
+      setInterval(function() {
+        data.setValue(1, 1, 40 + Math.round(60 * Math.random()));
+        chart.draw(data, options);
+      }, 1000);
+    }
+
