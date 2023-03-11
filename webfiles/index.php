89091e89190957d64c0816b95601e567f7d82621
Jonathan Broster <jonathan@broster.co.uk>
2023-03-11 14:03:59 +0000

    changes2

--- webfiles/index.php
+++ webfiles/index.php
@@ -34,7 +34,7 @@
              }
              else{
                 echo ("Yay! File is not effed!");
-                echo($_POST)
+                echo(var_dump($_POST))
              }
             $file = fopen("/home/hydropi/HydroHeat/webfiles/temps.txt", "w") or die ("DIE NOW!");
             fwrite($file, $_POST);
