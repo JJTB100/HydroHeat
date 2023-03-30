b3b586b7725a6631e77d269f9ba23c8370f26d19
Jonathan Broster <jonathan@broster.co.uk>
2023-03-30 20:09:21 +0100

    no more control page

--- webfiles/controlsLib.js
+++ webfiles/controlsLib.js
@@ -1,36 +1,3 @@
-var slider = document.getElementById("myRange");
-var flownum = document.getElementById("flow-rate-num");
-var flowRate = 50;
-var plus = document.getElementById("plus");
-var minus = document.getElementById("minus");
-updateFlow()
-
-slider.oninput = function() {
-    flowRate = this.value;
-    console.log(flowRate)
-    updateFlow();
-  }
-  
-  plus.onclick = function(){
-    //console.log(parseInt(flowRate));
-    if(flowRate < 100){
-      flowRate++;
-      updateFlow();
-    }
-    
-  }
-  minus.onclick = function(){
-    if(flowRate > 0){
-      flowRate--;
-      updateFlow();
-    }
-    
-  }
-
-  function updateFlow(){
-    flownum.innerText = flowRate.toString();
-    slider.value = flowRate;
-  }
 
 
 google.charts.load('current', {'packages':['gauge']});
@@ -48,7 +15,7 @@ function drawChart() {
   var options = {
     redFrom: 90, redTo: 100, redColor: '#D84727', greenColor: '#31AFD4', greenFrom: 0, greenTo: 75,
     yellowFrom:75, yellowTo: 90,
-    minorTicks: 5, width: 700, height: 300 ,
+    minorTicks: 5, width: 800, height: 400 ,
   };
 
   var chart = new google.visualization.Gauge(document.getElementById('temp-chart'));
