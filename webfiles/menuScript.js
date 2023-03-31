11d582c1101f894a5fbe98089f68b18aa69d87b5
Jonathan Broster <jonathan@broster.co.uk>
2023-03-31 10:21:15 +0100

    cleaning and smooth animations

--- webfiles/menuScript.js
+++ webfiles/menuScript.js
@@ -41,4 +41,16 @@ function drawChart() {
   }, 5000);
 }
 
-
+const observer = new IntersectionObserver((entries) =>{
+  entries.forEach((entry) =>{
+    console.log(entry)
+    if(entry.isIntersecting){
+      entry.target.classList.add('show');
+    }
+    else{
+      entry.target.classList.remove('show');
+    }
+  });
+})
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
